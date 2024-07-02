924d03e2fae5c5deb41e0d70dea82211c96ba415
GeekMomProjects <debraansell@yahoo.com>
2024-07-02 11:37:33 -0700

    Update level1Device.py
    Added button object functionality for multiple songs to iterate through, associated with different animation colors

--- examples/level1Device.py
+++ examples/level1Device.py
@@ -2,16 +2,16 @@ import board
 import busio
 import time
 import neopixel
+import random
+import neopixel
 from dyplayer import DYPlayer
 from digitalio import DigitalInOut, Direction, Pull
-import random
 from adafruit_debouncer import Debouncer
-import neopixel
 from adafruit_led_animation.animation.chase import Chase
 
 ### --- Create the Neopixel object and animation(s)
-num_pixels = 38
-pixels = neopixel.NeoPixel(board.GP0, num_pixels, brightness = 0.2, auto_write=False)
+num_pixels = 8
+pixels = neopixel.NeoPixel(board.GP27, num_pixels, brightness = 0.2, auto_write=False)
 pixels.fill((0,0,0))
 pixels.show()
 
@@ -42,11 +42,12 @@ player = DYPlayer(uart=player_uart)
 class ButtonObject:
     def __init__(self, name, pin, track=None, duration=3, color=None):
 
-        self.name = name                             # Text value for name - help with identifying button
+        self.name = name                        # Text value for name - help with identifying button
         self.pin = pin
-        self.color = color							  # RGB Color value - used for LED animations
-        self.track = track                            # File number of the sound
-        self.duration = duration                      # Duration (in seconds) to play the sound
+        self.color = color					    # RGB Color value - used for LED animations
+        self.track = track                      # File number or list of file numbers containing sounds
+        self.track_index = 0                    # index of the current track
+        self.duration = duration                # Duration (in seconds) or list of durations to play the sounds
 
         # Create a debounced button object
         self.pinobj = DigitalInOut(self.pin)
@@ -54,33 +55,49 @@ class ButtonObject:
         self.pinobj.pull = Pull.UP
         self.switch = Debouncer(self.pinobj)
 
+    # play_current_track retunrs the properties associated with the track
+    # to be played when the button is pushed, then, if there are a list of
+    # tracks associated with the button object, it advances the index of the
+    # current track to the next track in the list, and wraps around if the
+    # current track is the last track in the list
+    def play_current_track(self):
+        if type(self.track) is list:
+            t = self.track[self.track_index]
+            d = self.duration[self.track_index]
+            c = self.color[self.track_index]
+            self.track_index = (self.track_index + 1) % len(self.track)
+            return t,d,c
+        else:
+            return self.track
+
+    # Returns the file number of the current track associated with
+    # this button. This function does NOT advance the track index
+    def get_current_track(self):
+        if type(self.track) is list:
+            return self.track[self.track_index]
+        else:
+            return self.track
 
 
-### --- Create a ButtonObject object for each button on the board. Each button has a song, duration and 
-###     color associated with it. 
+### --- Create a ButtonObject object for each button on the board. Each button has a song, duration and
+###     color associated with it.
 
-PIN_A       = board.GP1      # Pi Pico pin attached to button
-SONG_A      = 1              # Track number of the song to play
-DURATION_A  = 5              # Duration in seconds of the song
-COLOR_A     = (0, 255, 0)    # Color of the animation to play with this song
+PIN_A       = board.GP16                # Pi Pico pin attached to button
+SONG_A      = [1,2]                     # Track number(s) of the song(s): e.g. [2] or [1,2,3] to play
+DURATION_A  = [60,20]                   # Duration in seconds of the song(s) to play: e.g. [45] or [60,45,30]
+COLOR_A     = [(0, 255, 0),(200,100,0)] # Color of the animation(s) to play with this song: e.g. [(255,0,0)] or [(255,0,0),(0,255,0),(128,255,0)]
 
-PIN_B       = board.GP2
-SONG_B      = 2
-DURATION_B  = 5
-COLOR_B      = (255, 0, 0)
+PIN_B       = board.GP17                              # Pi Pico pin attached to button
+SONG_B      = [3,4,5]                                 # Track number(s) of the song(s): e.g. [2] or [1,2,3] to play
+DURATION_B  = [60,25,30]                              # Duration in seconds of the song(s) to play: e.g. [45] or [60,45,30]
+COLOR_B     = [(0, 255, 0),(200,100,0),(0,200,200)]   # Color of the animation(s) to play with this song: e.g. [(255,0,0)] or [(255,0,0),(0,255,0),(128,255,0)]
 
-PIN_C       = board.GP4
-SONG_C      = 3
-DURATION_C  = 5
-COLOR_C      = (0, 0, 255)
 
 # list of button objects. Add as many buttons as you want to the list, you will not have to change
 # the code in the main loop to handle them
 button_objects = [ ButtonObject("Button A", PIN_A, track=SONG_A, duration=DURATION_A, color=COLOR_A),
-                   ButtonObject("Button B", PIN_B, track=SONG_B, duration=DURATION_B, color=COLOR_B),
-                   ButtonObject("Button C", PIN_C, track=SONG_C, duration=DURATION_C, color=COLOR_C)
-                 ]
-                 
+                   ButtonObject("Button B", PIN_B, track=SONG_B, duration=DURATION_B, color=COLOR_B)]
+
 ### --- End button creation
 
 # Variables to track the state of the player.
@@ -111,26 +128,27 @@ def stop_playing():
     sound_end_time = -1
 
 
-# Main loop. No need to change code here. To add new buttons, simply create new button objects 
+# Main loop. No need to change code here. To add new buttons, simply create new button objects
 while True:
     now = time.monotonic()
     if is_playing:
         chase.animate()
-    
+
     # Update all debounced buttons at the beginning of the loop
     for button in button_objects:
         button.switch.update()
-        
+
     # Check to see if there is a sound playing, and if it has exceeded the play duration
     if is_playing and now > sound_end_time:
         stop_playing()
-        
+
     # check to see if the user pressed any of the buttons.
     for button in button_objects:
         if button.switch.fell:                                  # Check to see if any of the buttons have been pressed
             print(button.name, " was pressed.")
-            if is_playing and button.track == current_track:    # User pressed same button twice and it was playing, so stop
+            if is_playing and button.get_current_track() == current_track:    # User pressed same button twice and it was playing, so stop
                 stop_playing()
             else:
-                start_playing(button.track, button.duration, button.color)
-            
+                t,d,c = button.play_current_track()
+                start_playing(t,d,c)
+
