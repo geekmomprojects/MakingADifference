41f71d2d4a56f928d53f6ab873e4d3b49ada9be7
GeekMomProjects <debraansell@yahoo.com>
2025-06-10 15:59:34 -0700

    Create action_led.py
    Added action class for "dumb" leds

---
+++ lib/action_object/action_led.py
@@ -0,0 +1,24 @@
+from action_object import Action
+import digitalio
+
+
+# An action class for an LED 
+class ActionLED(Action):
+    def __init__(self, ledPin):
+        self.led = digitalio.DigitalInOut(ledPin)
+		self.led.direction = digitalio.Direction.OUTPUT
+		self.led.value = False
+        super().__init__(name="LEDPin" + str(ledPin.__hash__))
+		
+	# Turn the LED on when action starts
+    def on_start(self):
+		self.led.value = True
+
+
+    # Turn the LED off when action starts
+    def on_stop(self):
+        self.led.value = False
+		
+	# No action needed - LED turns on/off with on_start, on_stop functions
+	def action(self):
+		return True
