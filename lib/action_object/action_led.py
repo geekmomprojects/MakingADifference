2e65cf28e471567bd06576a7b8d9cf13d80eabe4
GeekMomProjects <debraansell@yahoo.com>
2025-06-10 18:15:32 -0700

    changed spaces to tabs

--- lib/action_object/action_led.py
+++ lib/action_object/action_led.py
@@ -4,20 +4,19 @@ import digitalio
 
 # A simple action class for an LED that turns on for the duration of the action
 class ActionLED(Action):
-    def __init__(self, ledPin):
-        self.led = digitalio.DigitalInOut(ledPin)
+	def __init__(self, ledPin):
+		self.led = digitalio.DigitalInOut(ledPin)
 		self.led.direction = digitalio.Direction.OUTPUT
 		self.led.value = False
-        super().__init__(name="LEDPin" + str(ledPin.__hash__))
+		super().__init__(name="LEDPin" + str(ledPin.__hash__))
 		
 	# Turn the LED on when action starts
-    def on_start(self):
+	def on_start(self):
 		self.led.value = True
-
-
+		
 	# Turn the LED off when action stops
-    def on_stop(self):
-        self.led.value = False
+	def on_stop(self):
+		self.led.value = False
 		
 	# No action needed - LED turns on/off with on_start, on_stop functions
 	# but still must override this function for the class to work
